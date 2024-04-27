d9cdc6ca7d1278b521703825c2f9caada121da72
GARCIA ALVAREZ SILVIA ANAHI <I23050337@monclova.tecnm.mx>
2024-04-26 23:21:19 -0600

    Agregar archivos de proyecto.

---
+++ Unit6_appExcel/Employee_data.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit6_appExcel
+{
+	internal class Employee_data
+	{
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private string paternal_surname;
+
+		public string Paternal_surname
+		{
+			get { return paternal_surname; }
+			set { paternal_surname = value; }
+		}
+
+		private string mother_surname;
+
+		public string Mother_surname
+		{
+			get { return mother_surname; }
+			set { mother_surname = value; }
+		}
+
+		private int age;
+
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+
+		private int number_of_children;
+		public int Number_of_children
+		{
+			get { return number_of_children; }
+			set { number_of_children = value; }
+		}
+		 public Employee_data()
+		 {
+			this.name = "";
+			this.paternal_surname = "";
+			this.mother_surname = "";
+			this.age = 0;
+			this.number_of_children = 0;
+		 } 
+		public Employee_data(string name, string paternalsurname, string mothersurname,int age, int numberch)
+		 {
+			this.name = name;
+			this.paternal_surname = paternalsurname;
+			this.mother_surname = mothersurname;
+			this.age = age;
+			this.number_of_children = numberch;
+		 }
+
+		public override string ToString()
+		{
+			return name + " " + paternal_surname + " " + mother_surname + " " + age + " " + number_of_children;
+		}
+
+		
+
+
+	}
+}
---
+++ Unit6_appExcel/Program.cs
@@ -0,0 +1,99 @@
+﻿
+using Unit6_appExcel;
+using System.IO;
+using OfficeOpenXml;
+public class Program
+{
+	
+	static void Main(string[] args)
+	{
+		int employeeCount = 0;
+		int option;
+		Employee_data[] employees = new Employee_data[20];
+		
+		
+		do
+		{
+			Console.WriteLine("HUMAN RESOURCES");
+			Console.WriteLine("Choose an option \n1)enter information\n2)save file\n3)exitS");
+			option = int.Parse(Console.ReadLine());
+			switch (option)
+			{
+				case 1:
+
+					while (true)
+					{
+						if (employeeCount >= employees.Length)
+						{
+							Console.WriteLine("Ya has ingresado información para todos los empleados.");
+							break;
+						}
+
+						Console.WriteLine("Enter the name for employee");
+						string name = Console.ReadLine();
+						Console.WriteLine("Enter the paternal surname for employee");
+						string paternalSurname = Console.ReadLine();
+						Console.WriteLine("Enter the mother surname for employee");
+						string motherSurname = Console.ReadLine();
+						Console.WriteLine("Enter the age for employee");
+						int age = int.Parse(Console.ReadLine());
+						Console.WriteLine("Enter the number of children for employee");
+						int numberOfChildren = int.Parse(Console.ReadLine());
+
+						employees[employeeCount] = new Employee_data
+						{
+							Name = name,
+							Paternal_surname = paternalSurname,
+							Mother_surname = motherSurname,
+							Age = age,
+							Number_of_children = numberOfChildren
+						};
+						employeeCount++;
+
+						Console.WriteLine("Do you wish to enter information for another employee? (y/n)");
+						string response = Console.ReadLine().ToLower();
+						if (response != "y")
+						{
+							break; 
+						}
+					}
+					break;
+
+				case 2:
+					string path = @"C:\Users\Ani uwu\Desktop\SEMESTRE 2 POO\Unit 6\excel\Employee.xlsx"; 
+
+					using (var package = new ExcelPackage(new FileInfo(path))) 
+					{
+						var worksheet = package.Workbook.Worksheets.Add("Employee");
+						worksheet.Cells[1, 1].Value = "Name";
+						worksheet.Cells[1, 2].Value = "Paternal Surname";
+						worksheet.Cells[1, 3].Value = "Mother Surname";
+						worksheet.Cells[1, 4].Value = "Age";
+						worksheet.Cells[1, 5].Value = "Number of children";
+
+						int row = 2;
+						foreach (Employee_data emp in employees)
+						{
+							if (emp != null) 
+							{
+								worksheet.Cells[row, 1].Value = emp.Name;
+								worksheet.Cells[row, 2].Value = emp.Paternal_surname;
+								worksheet.Cells[row, 3].Value = emp.Mother_surname;
+								worksheet.Cells[row, 4].Value = emp.Age;
+								worksheet.Cells[row, 5].Value = emp.Number_of_children;
+								row++;
+							}
+						}
+						package.Save(); 
+					}
+					break;
+					default:
+					Console.WriteLine("Nothing");
+					break;
+			}
+
+		} while (option != 3);
+
+	}
+}
+
